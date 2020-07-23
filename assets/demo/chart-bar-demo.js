f9664775bcc88470daa8f860c260ff4945a0894c
kg1 <ki4017kk@gmail.com>
2020-07-23 17:15:25 +0900

    data updated 2020.7.23

--- assets/demo/chart-bar-demo.js
+++ assets/demo/chart-bar-demo.js
@@ -156,7 +156,7 @@ var myLineChart20 = new Chart(ctx20, {
         label: "大城",
         backgroundColor: "#ffc107",
         borderColor: "#ffc107",
-        data: [1, 7],
+        data: [1, 8],
       },
       {
         label: "その他",
