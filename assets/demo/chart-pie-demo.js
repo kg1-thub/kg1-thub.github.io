4c6a99ec018f1f7dcf2af00e2332145a7496fa02
kg1 <ki4017kk@gmail.com>
2020-08-26 22:45:03 +0900

    data updated 2020.8.26

--- assets/demo/chart-pie-demo.js
+++ assets/demo/chart-pie-demo.js
@@ -139,7 +139,7 @@ var myPieChart20v2 = new Chart(ctx20v2, {
   data: {
     labels: ["小林", "炭谷", "大城", "岸田"],
     datasets: [{
-      data: [2, 21, 29, 3],
+      data: [2, 21, 29,3],
       backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
     }],
   },
