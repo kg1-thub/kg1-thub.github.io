9c4d8f7c8ee6e7e633553e0457b7c5fd0b3f2f84
kg1 <ki4017kk@gmail.com>
2020-08-29 17:25:43 +0900

    data updated 2020.8.29

--- assets/demo/chart-pie-demo.js
+++ assets/demo/chart-pie-demo.js
@@ -120,7 +120,7 @@ var myPieChart20 = new Chart(ctx20, {
   data: {
     labels: ["小林", "炭谷", "大城", "岸田"],
     datasets: [{
-      data: [12, 186, 262, 43.33],
+      data: [12, 186, 269, 45.33],
       backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
     }],
   },
@@ -139,7 +139,7 @@ var myPieChart20v2 = new Chart(ctx20v2, {
   data: {
     labels: ["小林", "炭谷", "大城", "岸田"],
     datasets: [{
-      data: [2, 23, 29, 3],
+      data: [2, 23, 30, 3],
       backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
     }],
   },
