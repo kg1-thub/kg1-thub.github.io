73f4a8f33ce27e2f340eb6bd15b23df4dfd06fca
kg1 <ki4017kk@gmail.com>
2022-04-22 14:14:50 +0900

    3hits updated 2022.4.22

--- js/games.js
+++ js/games.js
@@ -14,6 +14,7 @@
     const hits2 = document.getElementById('hits2');
     var times = 0;
     const sheets = [];
+    let marks = [];
 
     num1.focus();
 
@@ -49,72 +50,75 @@
         sheets.push(sheet);
         console.log(sheets);
 
-        if (hits==3) {
-            hits1.textContent = `${hits} HITS!!!  WIN!!!`;
-            document.getElementById('resetbtn').hidden = false;
-            document.getElementById('sharebtn').hidden = false;
-            let texts = "http://twitter.com/share?url=kg1-thub.github.io/games/&text=3HITS. WIN!!!%0D%0A";
-            for (let j=0; j<times; j++) {
-                for (let i=0; i<3; i++) {
-                    if (sheets[j][i]==1){
-                        document.getElementById('sheet'+String(j+1)+String(i+1)).style.backgroundColor = 'yellowgreen';
-                        document.getElementById('sheet'+String(j+1)+String(i+1)).style.color = 'midnightblue';
-                        texts += '🟩';
-                    } else if (sheets[j][i]==0) {
-                        document.getElementById('sheet'+String(j+1)+String(i+1)).style.backgroundColor = 'khaki';
-                        document.getElementById('sheet'+String(j+1)+String(i+1)).style.color = 'midnightblue';
-                        texts += '🟨';
-                    } else {
-                        texts += '⬜';
-                    }
-                 }
-                 texts += '%0D%0A';
-            }
-            texts += '&hashtags=3HITS';
+        let k=0;
+        let ansnum = 0;
+        let answers = [];
+        let playinterval = null;
+        let texts = "http://twitter.com/share?url=kg1-thub.github.io/games/&text=";
+        let endflg=0;
 
-            let tweet = document.getElementById('tweetcontents');
-            tweet.setAttribute('href',texts);
+        if (hits==3) {
+            hits1.textContent = `${hits}HITS!!!  WIN!!!`;
+            texts = "3HITS. WIN!!!%0D%0A";
+            endflg = 1;
+            answers = ['resetbtn', 'sharebtn'];
         } else {
             hits1.textContent = `${hits} HITS, ${fouls} FOULS.`;
             if (times==5) {
                 hits2.textContent = `5 attempts. LOSE!!!`;
                 for (let i=0; i<3; i++) {
                     document.getElementById('answer'+String(i+1)).textContent=answer_key[i];
-                    document.getElementById('answer'+String(i+1)).hidden=false;
                 }
-                let texts = "http://twitter.com/share?url=kg1-thub.github.io/games/&text=NOT 3HITS. LOSE!%0D%0A";
-                for (let j=0; j<5; j++) {
-                    for (let i=0; i<3; i++) {
-                        if (sheets[j][i]==1){
-                            document.getElementById('sheet'+String(j+1)+String(i+1)).style.backgroundColor = 'yellowgreen';
-                            document.getElementById('sheet'+String(j+1)+String(i+1)).style.color = 'midnightblue';
-                            texts += '🟩';
-                        } else if (sheets[j][i]==0) {
-                            document.getElementById('sheet'+String(j+1)+String(i+1)).style.backgroundColor = 'khaki';
-                            document.getElementById('sheet'+String(j+1)+String(i+1)).style.color = 'midnightblue';
-                            texts += '🟨';
-                        } else {
-                            texts += '⬜';
-                        }
-                    }
-                    texts += '%0D%0A';
-                }
-                texts += '&hashtags=3HITS';
-
+                texts += "NOT 3HITS. LOSE!%0D%0A";
                 console.log(texts);
-                let tweet = document.getElementById('tweetcontents');
-                tweet.setAttribute('href',texts);
-
-                document.getElementById('answer').hidden = false;
-                document.getElementById('greena1').hidden = false;
-                document.getElementById('greena2').hidden = false;
-                document.getElementById('greena3').hidden = false;
-                document.getElementById('resetbtn').hidden = false;
-                document.getElementById('sharebtn').hidden = false;
-                // resetbtn.hidden = false;
-                // sharebtn.hidden = false;
+                endflg=1;
+                answers = ['answer', 'answer1', 'answer2', 'answer3', 'greena1', 'greena2', 'greena3', 'resetbtn', 'sharebtn'];
             }
         }
+
+        if (endflg>0){
+            for (let j=0; j<times; j++) {
+                for (let i=0; i<3; i++) {
+                    if (sheets[j][i]==1){
+                        texts += '🟩';
+                        marks.push([j, i]);
+                    } else if (sheets[j][i]==0) {
+                        texts += '🟨';
+                        marks.push([j, i]);
+                    } else {
+                        texts += '⬜';
+                    }
+                }
+                texts += '%0D%0A';
+            }
+            texts += '&hashtags=3HITS';
+
+            playinterval = setInterval(() => {
+                if(k==marks.length){
+                    if(ansnum==answers.length){
+                        clearInterval(playinterval);
+                    }
+                    document.getElementById(answers[ansnum]).hidden = false;
+                    ansnum++;
+                } else {
+                    let _marks = marks[k];
+                    let j = _marks[0];
+                    let i = _marks[1];
+                    if (sheets[j][i]==1){
+                        document.getElementById('sheet'+String(j+1)+String(i+1)).style.backgroundColor = 'yellowgreen';
+                        document.getElementById('sheet'+String(j+1)+String(i+1)).style.color = 'midnightblue';
+                    } else if (sheets[j][i]==0) {
+                        document.getElementById('sheet'+String(j+1)+String(i+1)).style.backgroundColor = 'khaki';
+                        document.getElementById('sheet'+String(j+1)+String(i+1)).style.color = 'midnightblue';
+                    }
+                    k++;
+                }
+            }, 150);
+        }
+
+
+        let tweet = document.getElementById('tweetcontents');
+        tweet.setAttribute('href',texts);
 
         for (let j=1; j<hits+1; j++){
             document.getElementById('green'+String(times)+String(j)).hidden = false;
