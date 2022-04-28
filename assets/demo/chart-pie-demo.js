2b8e2940475a6db7422136c0df90bc9f9ee7760d
kg1 <ki4017kk@gmail.com>
2022-04-28 21:39:02 +0900

    data updated 2022.4.28

--- assets/demo/chart-pie-demo.js
+++ assets/demo/chart-pie-demo.js
@@ -4,7 +4,7 @@
 
 var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-var pie_data22 = {'Catcher': ['大城', '小林', '山瀬', 'その他'], 'Stealing_bases': [3, 4, 0, 0], 'Defense_inning': [[35.0, 118.0, 0, 0, 0, 0, 0, 0], [20.0, 71.0, 0, 0, 0, 0, 0, 0], [0, 1.0, 0, 0, 0, 0, 0, 0], [0, 3.0, 0, 0, 0, 0, 0, 0]], 'Starting_games': [[4, 14, 0, 0, 0, 0, 0, 0], [2, 8, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]], 'Winning_games': [[3, 10, 0, 0, 0, 0, 0, 0], [2, 4, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]};
+var pie_data22 = {'Catcher': ['大城', '小林', '山瀬', 'その他'], 'Stealing_bases': [3, 4, 0, 0], 'Defense_inning': [[35.0, 118.0, 0, 0, 0, 0, 0, 0], [20.0, 71.0, 0, 0, 0, 0, 0, 0], [0, 1.0, 0, 0, 0, 0, 0, 0], [0, 12.0, 0, 0, 0, 0, 0, 0]], 'Starting_games': [[4, 14, 0, 0, 0, 0, 0, 0], [2, 8, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0]], 'Winning_games': [[3, 10, 0, 0, 0, 0, 0, 0], [2, 4, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0]]};
 var innings22 = pie_data22.Defense_inning;
 var starting_games22 = pie_data22.Starting_games;
 var stealing_bases22 = pie_data22.Stealing_bases;
