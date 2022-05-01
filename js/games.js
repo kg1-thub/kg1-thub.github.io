eba8617fc4e45c33da617325fe5806d2cbecec4c
kg1 <ki4017kk@gmail.com>
2022-05-02 00:41:10 +0900

    games updated 2022.5.1

--- js/games.js
+++ js/games.js
@@ -1,5 +1,78 @@
 'use strict';
 
+function clickSpin(times, target_hits, answer_key){
+    const hits1 = document.getElementById('hits1');
+    const hits2 = document.getElementById('hits2');
+    const spin = document.getElementById('spin'+String(target_hits));
+
+    const answer = [];
+    var hits=0;
+    var fouls=0;
+    let sheet = [];
+
+    for (let i=0; i<target_hits; i++) {
+        let k = parseInt(document.getElementById('num'+String(i+1)).value);
+        answer.push(k);
+        document.getElementById('sheet'+String(times)+String(i+1)).textContent=k;
+        document.getElementById('num'+String(i+1)).value="";
+        sheet.push(-1);
+    }
+    for (let i=0; i<target_hits; i++) {
+        for (let k=0; k<target_hits; k++){
+            if (answer[i]==answer_key[k]){
+                if (i==k) {
+                    hits++;
+                    sheet[i] = 1;
+                } else {
+                    fouls++;
+                    sheet[i] = 0;
+                }
+            }
+        }
+    }
+    // sheets.push(sheet);
+
+    let answers = [];
+    let texts = "http://twitter.com/share?url=https://kg1-thub.github.io/games/&text=";
+    let endflg=0;
+
+    for (let j=1; j<hits+1; j++){
+        document.getElementById('green'+String(times)+String(j)).hidden = false;
+    }
+
+    for (let j=1; j<fouls+1; j++){
+        document.getElementById('yellow'+String(times)+String(j)).hidden = false;
+    }
+
+    document.getElementById('time'+String(times)).hidden = false;
+
+    if (hits==target_hits) {
+        hits1.textContent = `${hits}HITS!!!  WIN!!!`;
+        console.log(hits1.textContent);
+        texts += `${hits}HITS. WIN!!!%0D%0A`;
+        endflg = 1;
+        answers = ['resetbtn', 'sharebtn'];
+    } else {
+        hits1.textContent = `${hits} HITS, ${fouls} BLOWS.`;
+        console.log(hits1.textContent);
+        if (times==target_hits*2-1) {
+            hits2.textContent = `Done ${times} attempts. LOSE!!!`;
+            for (let i=0; i<target_hits; i++) {
+                document.getElementById('answer'+String(i+1)).textContent=answer_key[i];
+            }
+            texts += `NOT ${target_hits}HITS. LOSE!!!%0D%0A`;
+            endflg=1;
+            if (target_hits==3){
+                answers = ['answer', 'answer1', 'answer2', 'answer3', 'resetbtn', 'sharebtn'];
+            }
+            if (target_hits==4){
+                answers = ['answer', 'answer1', 'answer2', 'answer3', 'answer4', 'resetbtn', 'sharebtn'];
+            }
+        }
+    }
+    return [endflg, sheet, texts, answers];
+}
+
 function reloadHits(target_hits){
     const answer_key = [];
     while (answer_key.length < target_hits) {
@@ -18,12 +91,10 @@ function reloadHits(target_hits){
     const up = document.getElementById('up');
     up.textContent = `Up to ${target_hits*2-1} attempts.`
 
-    const spin = document.getElementById('spin1');
-    const hits1 = document.getElementById('hits1');
-    const hits2 = document.getElementById('hits2');
-    var times = 0;
-    const sheets = [];
-    let marks = [];
+    document.getElementById('spin3').hidden = true;
+    document.getElementById('spin4').hidden = true;
+    const spin = document.getElementById('spin'+String(target_hits));
+    spin.hidden = false;
 
     num1.focus();
 
@@ -86,77 +157,35 @@ function reloadHits(target_hits){
             })
         }
     }
+    return answer_key;
+}
 
-    spin.addEventListener('click', () => {
-        times++;
+{
+    let target_hits = 4;
+    let answer_key = reloadHits(target_hits);
 
-        const answer = [];
-        var hits=0;
-        var fouls=0;
-        let sheet = [];
+    var times = 0;
+    const sheets = [];
+    let marks = [];
 
-        for (let i=0; i<target_hits; i++) {
-            let k = parseInt(document.getElementById('num'+String(i+1)).value);
-            answer.push(k);
-            document.getElementById('sheet'+String(times)+String(i+1)).textContent=k;
-            document.getElementById('num'+String(i+1)).value="";
-            sheet.push(-1);
-        }
+    // hits3btn.addEventListener('click', () => {
+    //     // location.reload();
+    //     answer_key = reloadHits(3);
+    // });
 
-        for (let i=0; i<target_hits; i++) {
-            for (let k=0; k<target_hits; k++){
-                if (answer[i]==answer_key[k]){
-                    if (i==k) {
-                        hits++;
-                        sheet[i] = 1;
-                    } else {
-                        fouls++;
-                        sheet[i] = 0;
-                    }
-                }
-            }
-        }
-        sheets.push(sheet);
-
+    spin4.addEventListener('click', () => {
         let k=0;
         let ansnum = 0;
-        let answers = [];
         let playinterval = null;
-        let texts = "http://twitter.com/share?url=https://kg1-thub.github.io/games/&text=";
-        let endflg=0;
 
-        for (let j=1; j<hits+1; j++){
-            document.getElementById('green'+String(times)+String(j)).hidden = false;
-        }
-
-        for (let j=1; j<fouls+1; j++){
-            document.getElementById('yellow'+String(times)+String(j)).hidden = false;
-        }
-
-        document.getElementById('time'+String(times)).hidden = false;
-
-        if (hits==target_hits) {
-            hits1.textContent = `${hits}HITS!!!  WIN!!!`;
-            texts += `${hits}HITS. WIN!!!%0D%0A`;
-            endflg = 1;
-            answers = ['resetbtn', 'sharebtn'];
-        } else {
-            hits1.textContent = `${hits} HITS, ${fouls} BLOWS.`;
-            if (times==target_hits*2-1) {
-                hits2.textContent = `Done ${times} attempts. LOSE!!!`;
-                for (let i=0; i<target_hits; i++) {
-                    document.getElementById('answer'+String(i+1)).textContent=answer_key[i];
-                }
-                texts += `NOT ${target_hits}HITS. LOSE!!!%0D%0A`;
-                endflg=1;
-                if (target_hits==3){
-                    answers = ['answer', 'answer1', 'answer2', 'answer3', 'resetbtn', 'sharebtn'];
-                }
-                if (target_hits==4){
-                    answers = ['answer', 'answer1', 'answer2', 'answer3', 'answer4', 'resetbtn', 'sharebtn'];
-                }
-            }
-        }
+        times++;
+        let result = clickSpin(times, 4, answer_key);
+        console.log(result);
+        let endflg = result[0];
+        let sheet = result[1];
+        let texts = result[2];
+        let answers = result[3];
+        sheets.push(sheet);
 
         if (endflg>0){
             for (let j=0; j<times; j++) {
@@ -203,12 +232,63 @@ function reloadHits(target_hits){
             num1.focus();
         }
     });
-}
 
-{
-    let target_hits = 4;
-    reloadHits(target_hits);
+    spin3.addEventListener('click', () => {
+        let k=0;
+        let ansnum = 0;
+        let playinterval = null;
 
-    // const modal = document.getElementById('modalbtn');
-    // modal.click();
+        times++;
+        let result = clickSpin(times, 3, answer_key);
+        let endflg = result[0];
+        let sheet = result[1];
+        let texts = result[2];
+        let answers = result[3];
+        sheets.push(sheet);
+
+        if (endflg>0){
+            for (let j=0; j<times; j++) {
+                for (let i=0; i<target_hits; i++) {
+                    if (sheets[j][i]==1){
+                        texts += '🟩';
+                        marks.push([j, i]);
+                    } else if (sheets[j][i]==0) {
+                        texts += '🟨';
+                        marks.push([j, i]);
+                    } else {
+                        texts += '⬜';
+                    }
+                }
+                texts += '%0D%0A';
+            }
+            texts += `&hashtags=${target_hits}HITS`;
+
+            playinterval = setInterval(() => {
+                if(k==marks.length){
+                    if(ansnum==answers.length-1){
+                        clearInterval(playinterval);
+                    }
+                    document.getElementById(answers[ansnum]).hidden = false;
+                    ansnum++;
+                } else {
+                    let _marks = marks[k];
+                    let j = _marks[0];
+                    let i = _marks[1];
+                    if (sheets[j][i]==1){
+                        document.getElementById('sheet'+String(j+1)+String(i+1)).style.backgroundColor = 'yellowgreen';
+                        document.getElementById('sheet'+String(j+1)+String(i+1)).style.color = 'midnightblue';
+                    } else if (sheets[j][i]==0) {
+                        document.getElementById('sheet'+String(j+1)+String(i+1)).style.backgroundColor = 'khaki';
+                        document.getElementById('sheet'+String(j+1)+String(i+1)).style.color = 'midnightblue';
+                    }
+                    k++;
+                }
+            }, 150);
+            let tweet = document.getElementById('tweetcontents');
+            tweet.setAttribute('href',texts);
+            spin.blur();
+        } else {
+            num1.focus();
+        }
+    });
 }
