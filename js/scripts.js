01e7c689abaada7a0f50508b4ed5ffb49bf9c3d5
kg1 <ki4017kk@gmail.com>
2022-05-24 21:34:14 +0900

    data updated.

--- js/scripts.js
+++ js/scripts.js
@@ -147,3 +147,40 @@ function maxValue(ctx) {
     }
     return max;
 };
+
+//Get the button:
+topbtn = document.getElementById("top-btn");
+downbtn = document.getElementById("down-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    topbtn.style.display = "block";
+  } else {
+    topbtn.style.display = "none";
+  }
+};
+
+topbtn.addEventListener("click", backToTop);
+downbtn.addEventListener("click", goToGames);
+
+// When the user clicks on the button, scroll to the top of the document
+function backToTop() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+function goToGames() {
+    const target = document.getElementById('games22');
+    const targetOffsetTop = window.pageYOffset + target.getBoundingClientRect().top - ($('.sb-topnav').height() + 8 * 2);
+    window.scrollTo({
+        top: targetOffsetTop,
+        behavior: "smooth"
+    });
+}
