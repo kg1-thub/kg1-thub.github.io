46dabe88c98cec35e1a17d8e361bdbf583e265f8
kg1 <ki4017kk@gmail.com>
2022-06-29 22:45:42 +0900

    data updated.

--- assets/demo/calendar-heatmap-demo.js
+++ assets/demo/calendar-heatmap-demo.js
@@ -79,6 +79,7 @@ var datas22 = {
     '2022-06-25': 1,
     '2022-06-26': -1,
     '2022-06-28': 1,
+    '2022-06-29': -1,
     //@@NEXTGAME@@
 };
 var score22 = {
@@ -159,6 +160,7 @@ var score22 = {
     '2022-06-25': 'vs Swallows , 5-19 </br>Start P.ｼｭｰﾒｰｶｰ / C.大城',
     '2022-06-26': 'vs Swallows , 11-10 </br>Start P.戸郷 / C.大城',
     '2022-06-28': 'vs Dragons , 6-2 </br>Start P.高橋 / C.大城',
+    '2022-06-29': 'vs Dragons , 2-3 </br>Start P.山崎伊 / C.大城',
     //@@NEXTSCORE@@
 };
 var chartData22 = [];
