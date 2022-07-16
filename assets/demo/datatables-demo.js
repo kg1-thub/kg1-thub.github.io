1a71989ea70b1c9916ac53a5325823de09e5d168
kg1 <ki4017kk@gmail.com>
2022-07-16 18:20:18 +0900

    ad updated.

--- assets/demo/datatables-demo.js
+++ assets/demo/datatables-demo.js
@@ -332,7 +332,7 @@ function makeCSV(records, columns) {
         searching: true,
         search: {
           regex: true,
-          search: "07/15" // @@KEYWORD@@
+          search: "07/16" // @@KEYWORD@@
         },
         paging: true,
         info: false,
