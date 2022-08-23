e0d23cb805d0a4bfa2f06fec756bb0a3d3735a7a
kg1 <ki4017kk@gmail.com>
2022-08-23 21:31:43 +0900

    data updated.

--- assets/demo/calendar-heatmap-demo.js
+++ assets/demo/calendar-heatmap-demo.js
@@ -116,6 +116,7 @@ var datas22 = {
     '2022-08-19': -1,
     '2022-08-20': -1,
     '2022-08-21': -1,
+    '2022-08-23': 1,
     //@@NEXTGAME@@
 };
 var score22 = {
@@ -233,6 +234,7 @@ var score22 = {
     '2022-08-19': 'vs Tigers , 0-4 </br>Start P.山崎伊 / C.大城',
     '2022-08-20': 'vs Tigers , 1-5 </br>Start P.堀田 / C.喜多',
     '2022-08-21': 'vs Tigers , 1-6 </br>Start P.ｼｭｰﾒｰｶｰ / C.大城',
+    '2022-08-23': 'vs Dragons , 6-0 </br>Start P.菅野 / C.大城',
     //@@NEXTSCORE@@
 };
 var chartData22 = [];
