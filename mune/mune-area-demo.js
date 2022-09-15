e124f92f8cc1d6fb90db9430ea35c63eda3dc5eb
kg1 <ki4017kk@gmail.com>
2022-09-16 00:23:34 +0900

    data updated.

---
+++ mune/mune-area-demo.js
@@ -0,0 +1,395 @@
+var ctx22_data = {
+  'Giants':   [0, 1, 2, 1, 2, 3, 4, 5, 6, 7, 6, 7, 6, 5,  6,  5,  6,  7,  7,  6,  5,  6,  7,  8,  9, 10,
+     11, 10,  9, 10, 11, 10,  9,  8,  7,  8,  7,  6,  5,  4,  3,  3,  4,  5,  6,  5,  6,  7,  8,  9,  8,
+      7,  8,  9,  8,  7,  8,  7,  6,  5,  6,  7,  8,  7,  6,  5,  6,  7,  6,  5,  4,  3,  4,  3,  4,  3,
+      2,  3,  2,  3,  2,  2,  1,  0,  1,  2,  3,  2,  2,  1,  0,  1,  0, -1, -2, -3, -4, -3, -4, -5, -5,
+     -5, -5, -5, -5, -5, -6, -7, -6, -5, -4, -3, -4, -3, -2, -3, -2, -1, -2, -3, -4, -5, -6, -7, -6, -7,
+     -6, -5, -6, -7, -8, -8, -8, -8, -7, -6, -7, -6, -5, -6, -7, -6, -6, -5, -5],
+  'Tigers':   [0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-8,-9,-9,-9,-10,-11,-12,-13,-14,-13,-12,-13,-14,-15,-16,-15,
+    -16,-15,-15,-14,-13,-12,-11,-10,-11,-12,-11,-12,-13,-12,-13,-14,-14,-14,-13,-12,-13,-12,-13,-14,-13,
+    -12,-11,-12,-13,-12,-11,-12,-13,-12,-11,-10, -9, -8, -7, -8, -9, -8, -7, -6, -5, -4, -5, -6, -7, -7,
+     -6, -5, -4, -5, -6, -7, -8, -7, -6, -6, -5, -6, -5, -5, -5, -6, -5, -4, -3, -4, -3, -3, -2, -3, -2,
+     -1,  0,  1,  2,  1,  2,  3,  2,  3,  2,  3,  2,  1,  0, -1, -2, -3, -4, -5, -4, -3, -2, -1, -2, -3,
+     -2, -3, -2, -3, -2, -1,  0,  0, -1, -2, -3, -2, -2, -3, -4, -3, -2, -3, -2],
+  'Baystars': [0,-1,-2,-3,-2,-1, 0, 1, 0, 0,-1, 0, 0, 0,  0,  0, -1, -2, -2, -3, -2, -3, -2, -1,  0, -1,
+     -2, -3, -2, -3, -4, -4, -5, -5, -6, -5, -4, -3, -4, -5, -4, -4, -5, -5, -6, -7, -6, -5, -5, -4, -5,
+     -6, -5, -6, -5, -4, -5, -6, -5, -6, -7, -6, -5, -6, -5, -6, -5, -6, -5, -6, -7, -8, -7, -6, -7, -6,
+     -7, -8, -9, -8, -7, -6, -5, -6, -7, -7, -6, -6, -6, -5, -4, -3, -4, -3, -3, -2, -1,  0, -1,  0, -1,
+     -2, -3, -3, -3, -3, -2, -1, -1,  0,  1,  0,  1,  2,  3,  4,  4,  3,  4,  5,  6,  7,  8,  9, 10, 11,
+     10,  9,  8,  7,  8,  9, 10,  9,  8,  7,  8,  7,  6,  7,  8,  7,  8,  9,  8],
+  'Swallows': [0, 1, 2, 3, 2, 1, 0,-1, 0, 0,-1, 0,-1, 0, -1,  0,  1,  0,  0,  1,  0,  1,  2,  1,  1,  0,
+      1,  0,  0,  1,  2,  2,  3,  3,  4,  5,  4,  5,  6,  7,  6,  5,  6,  6,  7,  7,  8,  7,  8,  7,  8,
+      9, 10, 11, 10, 11, 12, 11, 12, 11, 12, 13, 14, 15, 14, 15, 16, 17, 18, 19, 20, 21, 22, 21, 22, 23,
+     24, 23, 24, 25, 26, 27, 26, 27, 28, 27, 26, 27, 26, 26, 26, 26, 25, 24, 24, 23, 22, 21, 22, 23, 22,
+     21, 22, 21, 20, 21, 22, 23, 23, 22, 21, 20, 19, 18, 17, 16, 16, 17, 18, 19, 18, 17, 18, 17, 18, 19,
+     18, 19, 20, 21, 22, 22, 22, 23, 22, 21, 22, 21, 21, 22, 21, 22, 21, 20, 20],
+  'Dragons':  [0,-1,-2,-1,-2,-3,-4,-3,-2,-1, 0,-1, 0, 0,  0,  0,  1,  2,  3,  3,  2,  3,  2,  3,  3,  2,
+      1,  2,  2,  1,  0, -1,  0,  1,  2,  1,  0,  1,  2,  1,  2,  3,  2,  1,  0,  1,  0, -1, -1, -2, -3,
+     -4, -5, -6, -5, -4, -3, -4, -3, -4, -3, -4, -3, -2, -3, -4, -5, -6, -7, -8, -7, -6, -7, -6, -7, -8,
+     -9,-10,-11,-12,-11,-11,-10,-11,-12,-12,-13,-13,-12,-13,-12,-12,-11,-10,-11,-10,-11,-12,-11,-12,-12,
+    -12,-12,-11,-10, -9,-10,-11,-11,-12,-13,-12,-11,-12,-13,-12,-11,-10,-11,-12,-11,-10,-11,-10,-11,-10,
+    -11,-10,-11,-10,-11,-12,-13,-14,-13,-12,-13,-14,-13,-12,-11,-12,-13,-14,-13],
+  'Carp':     [0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 4, 3, 4, 4,  5,  6,  5,  6,  6,  6,  7,  6,  5,  4,  3,  4,
+      5,  6,  6,  5,  4,  5,  4,  3,  4,  3,  4,  3,  4,  5,  6,  7,  7,  7,  6,  6,  5,  4,  3,  4,  5,
+      6,  5,  6,  5,  4,  3,  2,  3,  2,  3,  2,  1,  0,  1,  0, -1,  0, -1, -2, -3, -4, -5, -4, -3, -3,
+     -2, -1,  0, -1, -2, -3, -2, -1, -2, -2, -3, -2, -3, -2, -3, -4, -3, -4, -3, -2, -1, -1, -2, -1,  0,
+      1,  0, -1, -2, -3, -4, -5, -5, -6, -5, -6, -5, -4, -3, -2, -3, -4, -3, -2, -3, -4, -5, -6, -7, -8,
+     -7, -8, -7, -6, -7, -8, -9, -8, -7, -6, -5, -4, -5, -6, -5, -6, -6, -5, -6],
+  'labels':[
+    'Start','03/25','03/26','03/27','03/29','03/30','03/31','04/01','04/02','04/03','04/05','04/06',
+    '04/07','04/08','04/09','04/10','04/12','04/13','04/14','04/15','04/16','04/17','04/19','04/20',
+    '04/21','04/22','04/23','04/24','04/26','04/27','04/28','04/29','04/30','05/01','05/03','05/04',
+    '05/05','05/06','05/07','05/08','05/10','05/11','05/12','05/13','05/14','05/15','05/17','05/18',
+    '05/19','05/20','05/21','05/22','05/24','05/25','05/26','05/27','05/28','05/29','05/31','06/01',
+    '06/02','06/03','06/04','06/05','06/07','06/08','06/09','06/10','06/11','06/12','06/17','06/18',
+    '06/19','06/21','06/22','06/23','06/24','06/25','06/26','06/28','06/29','06/30','07/01','07/02',
+    '07/03','07/05','07/06','07/07','07/08','07/09','07/10','07/12','07/13','07/14','07/15','07/16',
+    '07/17','07/18','07/19','07/20','07/22','07/23','07/24','07/29','07/30','07/31','08/02','08/03',
+    '08/04','08/05','08/06','08/07','08/09','08/10','08/11','08/12','08/13','08/14','08/16','08/17',
+    '08/18','08/19','08/20','08/21','08/23','08/24','08/25','08/26','08/27','08/28','08/30','08/31',
+    '09/01','09/02','09/03','09/04','09/06','09/07','09/08','09/09','09/10','09/11','09/12','09/13',
+    '09/14','09/15',
+    //'09/16',
+    //'09/17',
+    //'09/18',
+    //'09/20',
+    //'09/21',
+    //'09/22',
+    //'09/23',
+    //'09/24',
+    //'09/25',
+    //'09/26',
+    //'09/27',
+    //'09/28',
+  ],
+};
+
+// Annotation
+const zero = {
+  type: 'line',
+  scaleID: 'y',
+  borderWidth: 2,
+  borderColor: 'gray',
+  value: 0,
+};
+
+const illabel22 = {
+  type: 'line',
+  scaleID: 'x',
+  borderWidth: 0,
+  borderColor: 'rgba(255,182,193,0)',
+  value: 61,
+  label: {
+    // rotation: 270,
+    backgroundColor: 'pink',
+    color: 'black',
+    content: 'INTERLEAGUE PLAY',
+    enabled: true,
+    position: 'end',
+    font: {
+      size: 12
+    }
+  }
+};
+
+const ilperiod22 = {
+  type: 'box',
+  backgroundColor: 'rgba(255,182,193,0.35)',
+  borderColor: 'rgba(0,150,0,0)',
+  borderWidth: 1,
+  cornerRadius: 4,
+  xMin: 51,
+  xMax: 69,
+};
+
+const illabel21 = {
+  type: 'line',
+  scaleID: 'x',
+  borderWidth: 0,
+  borderColor: 'rgba(255,182,193,0)',
+  value: 61,
+  label: {
+    // rotation: 270,
+    backgroundColor: 'pink',
+    color: 'black',
+    content: 'INTERLEAGUE PLAY',
+    enabled: true,
+    position: 'start',
+    font: {
+      size: 12
+    }
+  }
+};
+
+const ilperiod21 = {
+  type: 'box',
+  backgroundColor: 'rgba(255,182,193,0.35)',
+  borderColor: 'rgba(0,150,0,0)',
+  borderWidth: 1,
+  cornerRadius: 4,
+  xMin: 51,
+  xMax: 72,
+};
+
+const olbreak21 = {
+  type: 'line',
+  scaleID: 'x',
+  borderWidth: 10,
+  borderColor: 'rgba(0,0,128,0.5)',
+  value: 97,
+  label: {
+    // rotation: 90,
+    backgroundColor: 'rgba(0,0,128,1)',
+    color: 'white',
+    content: 'OLYMPIC BREAK',
+    enabled: true,
+    position: 'start',
+    font: {
+      size: 12
+    }
+  }
+};
+
+const out_n22 = {
+  type: 'line',
+  scaleID: 'x',
+  borderWidth: 3,
+  borderColor: 'blue',
+  value: 11,
+  label: {
+    // rotation: 270,
+    backgroundColor: 'blue',
+    content: '#22 out [to Farm]',
+    enabled: true,
+    position: 'end',
+    font: {
+      size: 8
+    }
+  }
+};
+
+const in_n22 = {
+  type: 'line',
+  scaleID: 'x',
+  borderWidth: 3,
+  borderColor: 'blue',
+  value: 59,
+  label: {
+    // rotation: 270,
+    backgroundColor: 'blue',
+    content: '#22 in [from Farm]',
+    enabled: true,
+    position: 'end',
+    font: {
+      size: 8
+    }
+  }
+};
+
+const out_n27 = {
+  type: 'line',
+  scaleID: 'x',
+  borderWidth: 3,
+  borderColor: 'red',
+  value: 87,
+  label: {
+    // rotation: 270,
+    backgroundColor: 'red',
+    content: '#27 out [to Eagles]',
+    enabled: true,
+    position: 'end',
+    font: {
+      size: 8
+    }
+  }
+};
+
+const illabel19 = {
+  type: 'line',
+  scaleID: 'x',
+  borderWidth: 0,
+  borderColor: 'rgba(255,182,193,0)',
+  value: 61,
+  label: {
+    // rotation: 270,
+    backgroundColor: 'pink',
+    color: 'black',
+    content: 'INTERLEAGUE PLAY',
+    enabled: true,
+    position: 'start',
+    font: {
+      size: 12
+    }
+  }
+};
+
+const ilperiod19 = {
+  type: 'box',
+  backgroundColor: 'rgba(255,182,193,0.35)',
+  borderColor: 'rgba(0,150,0,0)',
+  borderWidth: 1,
+  cornerRadius: 4,
+  xMin: 51,
+  xMax: 69,
+};
+
+const illabel18 = {
+  type: 'line',
+  scaleID: 'x',
+  borderWidth: 0,
+  borderColor: 'rgba(255,182,193,0)',
+  value: 61,
+  label: {
+    // rotation: 270,
+    backgroundColor: 'pink',
+    color: 'black',
+    content: 'INTERLEAGUE PLAY',
+    enabled: true,
+    position: 'start',
+    font: {
+      size: 12
+    }
+  }
+};
+
+const ilperiod18 = {
+  type: 'box',
+  backgroundColor: 'rgba(255,182,193,0.35)',
+  borderColor: 'rgba(0,150,0,0)',
+  borderWidth: 1,
+  cornerRadius: 4,
+  xMin: 46,
+  xMax: 64,
+};
+
+// Area Chart 2022
+var ctx22x = document.getElementById("munemyAreaChart22");
+var myLineChart22x = new Chart(ctx22x, {
+  type: 'line',
+  data: {
+    labels: ctx22_data.labels,
+    datasets: [
+      {
+        label: "Giants",
+        hidden: false,
+        fill: 'origin',
+        lineTension: 0,
+        backgroundColor: "rgba(255,140,0,0.1)",
+        borderColor: "#EB9E16",
+        borderWidth: 5,
+        pointRadius: 0.3,
+        pointBackgroundColor: "#EB9E16",
+        pointBorderColor: "#EB9E16",
+        pointHoverRadius: 0.3,
+        pointHoverBackgroundColor: "#EB9E16",
+        pointHitRadius: 50,
+        pointborderWidth: 5,
+        data: ctx22_data.Giants,
+      }, // Giants
+      {
+        label: "Tigers",
+        hidden: false,
+        fill: 'origin',
+        lineTension: 0,
+        backgroundColor: "rgba(255,215,0,0.05)",
+        borderColor: "#F1EA49",
+        borderWidth: 5,
+        pointRadius: 0.3,
+        pointBackgroundColor: "#F1EA49",
+        pointBorderColor: "#F1EA49",
+        pointHoverRadius: 0.3,
+        pointHoverBackgroundColor: "#F1EA49",
+        pointHitRadius: 50,
+        pointborderWidth: 5,
+        data: ctx22_data.Tigers,
+      }, // Tigers
+      {
+        label: "Baystars",
+        hidden: false,
+        fill: 'origin',
+        lineTension: 0,
+        backgroundColor: "rgba(135,206,250,0.05)",
+        borderColor: "#3893D2",
+        borderWidth: 5,
+        pointRadius: 0.3,
+        pointBackgroundColor: "#3893D2",
+        pointBorderColor: "#3893D2",
+        pointHoverRadius: 0.3,
+        pointHoverBackgroundColor: "#3893D2",
+        pointHitRadius: 50,
+        pointborderWidth: 5,
+        data: ctx22_data.Baystars,
+      }, // Baystars
+      {
+        label: "Swallows",
+        hidden: false,
+        fill: 'origin',
+        lineTension: 0,
+        backgroundColor: "rgba(51,204,0,0.05)",
+        borderColor: "#5DD95D",
+        borderWidth: 5,
+        pointRadius: 0.3,
+        pointBackgroundColor: "#5DD95D",
+        pointBorderColor: "#5DD95D",
+        pointHoverRadius: 0.3,
+        pointHoverBackgroundColor: "#5DD95D",
+        pointHitRadius: 50,
+        pointborderWidth: 5,
+        data: ctx22_data.Swallows,
+      }, // Swallows
+      {
+        label: "Dragons",
+        hidden: false,
+        fill: 'origin',
+        lineTension: 0,
+        backgroundColor: "rgba(0,0,128,0.05)",
+        borderColor: "#6F3BC8",
+        borderWidth: 5,
+        pointRadius: 0.3,
+        pointBackgroundColor: "#6F3BC8",
+        pointBorderColor: "#6F3BC8",
+        pointHoverRadius: 0.3,
+        pointHoverBackgroundColor: "#6F3BC8",
+        pointHitRadius: 50,
+        pointborderWidth: 5,
+        data: ctx22_data.Dragons,
+      }, // Dragons
+      {
+        label: "Carp",
+        hidden: false,
+        fill: 'origin',
+        lineTension: 0,
+        backgroundColor: "rgba(255,0,0,0.05)",
+        borderColor: "#EF3A38",
+        borderWidth: 5,
+        pointRadius: 0.3,
+        pointBackgroundColor: "#EF3A38",
+        pointBorderColor: "#EF3A38",
+        pointHoverRadius: 0.3,
+        pointHoverBackgroundColor: "#EF3A38",
+        pointHitRadius: 50,
+        pointborderWidth: 5,
+        data: ctx22_data.Carp,
+      }, // Carp
+    ],
+  },
+  options: {
+    plugins: {
+      autocplors: false,
+      annotation: {
+        annotations: {
+          zero,
+          ilperiod22,
+          illabel22,
+        }
+      },
+      legend: {
+        position: 'right',
+        labels: {
+          boxWidth: 12
+        }
+      }
+    },
+    responsive: true,
+  }
+});
---
+++ mune/wk_mune.py
@@ -0,0 +1,31 @@
+import csv
+
+with open('mune.csv') as f:
+    reader = csv.reader(f)
+    gs = [row for row in reader]
+    print()
+
+games = {}
+score = {}
+for _g in gs[1:]:
+    # print(_g)
+    pt = 0
+    if int(_g[4])>0:
+        pt+=1
+    if int(_g[7])>0:
+        pt+=1
+    if int(_g[8])>0:
+        pt+=1
+    if pt==3:
+        if int(_g[7])>1:
+            pt +=2
+        else:
+            if int(_g[4])>1:
+                pt +=1
+            if int(_g[8])>1:
+                pt +=1
+    games[_g[0]] = pt
+    score[_g[0]] = ''+str(_g[4])+' Hits for '+str(_g[3])+' AtBats'+'</br>'+str(_g[7])+' HR, '+str(_g[8])+' RBI'
+
+print(games)
+print(score)
