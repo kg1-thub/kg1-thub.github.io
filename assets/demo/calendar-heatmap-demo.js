2a9123278ce108061217fba2466d0cf86b451ddd
kg1 <ki4017kk@gmail.com>
2022-09-19 17:01:03 +0900

    data updated.

--- assets/demo/calendar-heatmap-demo.js
+++ assets/demo/calendar-heatmap-demo.js
@@ -136,6 +136,7 @@ var datas22 = {
     '2022-09-13': 1,
     '2022-09-17': 1,
     '2022-09-18': -1,
+    '2022-09-19': 1,
     //@@NEXTGAME@@
 };
 var score22 = {
@@ -273,6 +274,7 @@ var score22 = {
     '2022-09-13': 'vs Swallows , 7-9 </br>Start P.菅野 / C.大城',
     '2022-09-17': 'vs Tigers , 3-2 </br>Start P.戸郷 / C.大城',
     '2022-09-18': 'vs Baystars , 1-3 </br>Start P.山崎伊 / C.大城',
+    '2022-09-19': 'vs Baystars , 5-3 </br>Start P.菅野 / C.大城',
     //@@NEXTSCORE@@
 };
 var chartData22 = [];
