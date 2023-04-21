e2201538ed0c53775551ee5dd1ab5bf0acb30b2c
kg1 <ki4017kk@gmail.com>
2023-04-21 21:35:56 +0900

    score update 2023.04.21

--- assets/demo/calendar-heatmap-demo.js
+++ assets/demo/calendar-heatmap-demo.js
@@ -20,6 +20,7 @@ var datas23 = {
     '2023-04-16': -1,
     '2023-04-18': -1,
     '2023-04-19': 1,
+    '2023-04-21': -1,
     //@@NEXTGAME@@
 };
 var score23 = {
@@ -40,6 +41,7 @@ var score23 = {
     '2023-04-16': 'vs Dragons , 7-5 </br>Start P.赤星 / C.大城',
     '2023-04-18': 'vs Baystars , 0-2 </br>Start P.戸郷 / C.大城',
     '2023-04-19': 'vs Baystars , 5-1 </br>Start P.山﨑 / C.大城',
+    '2023-04-21': 'vs Swallows , 3-0 </br>Start P.ビーディ / C.大城',
     //@@NEXTSCORE@@
 };
 var chartData23 = [];
