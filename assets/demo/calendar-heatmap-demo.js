68a66a32ea1d3614704c15e6f8c4cd61d8883b86
kg1 <ki4017kk@gmail.com>
2023-08-02 21:29:57 +0900

    score update 2023.08.02

--- assets/demo/calendar-heatmap-demo.js
+++ assets/demo/calendar-heatmap-demo.js
@@ -98,6 +98,7 @@ var datas23 = {
     '2023-07-29': 1,
     '2023-07-30': 1,
     '2023-08-01': -1,
+    '2023-08-02': 1,
     //@@NEXTGAME@@
 };
 var score23 = {
@@ -196,6 +197,7 @@ var score23 = {
     '2023-07-29': 'vs Dragons , 6-3 </br>Start P.山崎 / C.大城',
     '2023-07-30': 'vs Dragons , 4-0 </br>Start P.メンデス / C.岸田',
     '2023-08-01': 'vs Swallows , 0-1 </br>Start P.菅野 / C.大城',
+    '2023-08-02': 'vs Swallows , 9-2 </br>Start P.グリフィン / C.大城',
     //@@NEXTSCORE@@
 };
 var chartData23 = [];
