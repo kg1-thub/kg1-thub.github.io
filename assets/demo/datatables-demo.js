a470a8977b38c8f57a24dfe2454a85f20238543b
kg1 <ki4017kk@gmail.com>
2023-08-25 22:07:36 +0900

    score update 2023.08.25

--- assets/demo/datatables-demo.js
+++ assets/demo/datatables-demo.js
@@ -370,7 +370,7 @@ function makeCSV(records, columns, year) {
 
   var keyword="";
   if (year='23') {
-    keyword = "08/24" // @@KEYWORD@@
+    keyword = "08/25" // @@KEYWORD@@
   }
 
   $(document).ready(function() {
