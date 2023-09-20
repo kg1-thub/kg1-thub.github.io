29a6d9c8aef7c356337c75746c39d59400ae2bd7
kg1 <ki4017kk@gmail.com>
2023-09-20 21:45:20 +0900

    score update 2023.09.19

--- assets/demo/calendar-heatmap-demo.js
+++ assets/demo/calendar-heatmap-demo.js
@@ -139,6 +139,7 @@ var datas23 = {
     '2023-09-16': -1,
     '2023-09-17': 1,
     '2023-09-18': 1,
+    '2023-09-20': -1,
     //@@NEXTGAME@@
 };
 var score23 = {
@@ -278,6 +279,7 @@ var score23 = {
     '2023-09-16': 'vs Dragons , 1-0 </br>Start P.菅野 / C.大城',
     '2023-09-17': 'vs Swallows , 3-2 </br>Start P.メンデス / C.大城',
     '2023-09-18': 'vs Swallows , 4-3 </br>Start P.グリフィン / C.大城',
+    '2023-09-20': 'vs Tigers , 4-3 </br>Start P.山崎 / C.大城',
     //@@NEXTSCORE@@
 };
 var chartData23 = [];
--- assets/demo/datatables-demo.js
+++ assets/demo/datatables-demo.js
@@ -370,7 +370,7 @@ function makeCSV(records, columns, year) {
 
   var keyword="";
   if (year='23') {
-    keyword = "09/18"; // @@KEYWORD@@
+    keyword = "09/20"; // @@KEYWORD@@
     $(document).ready(function() {
       $(`#dt${year}games`).DataTable({
         order: [[ 0, "asc" ]],
