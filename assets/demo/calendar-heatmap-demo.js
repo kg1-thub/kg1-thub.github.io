47c8deeaadf39107c89648120674b922a677a508
kg1 <ki4017kk@gmail.com>
2024-05-08 21:16:11 +0900

    score update 2024.05.08

--- assets/demo/calendar-heatmap-demo.js
+++ assets/demo/calendar-heatmap-demo.js
@@ -37,6 +37,7 @@ var datas24 = {
     '2024-05-05': -1,
     '2024-05-06': -1,
     '2024-05-07': 1,
+    '2024-05-08': 1,
     //@@NEXTGAME@@
 };
 var score24 = {
@@ -74,6 +75,7 @@ var score24 = {
     '2024-05-05': 'vs Tigers , 2-4 </br>Start P.高橋 / C.小林',
     '2024-05-06': 'vs Dragons , 2-0 </br>Start P.横川 / C.小林',
     '2024-05-07': 'vs Dragons , 0-2 </br>Start P.山崎 / C.岸田',
+    '2024-05-08': 'vs Dragons , 1-4 </br>Start P.堀田 / C.岸田',
     //@@NEXTSCORE@@
 };
 var chartData24 = [];
--- assets/demo/chart-pie-demo.js
+++ assets/demo/chart-pie-demo.js
@@ -4,7 +4,7 @@
 
 var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-var pie_data24 = {'Catcher': ['大城', '小林', '岸田', 'その他'], 'stolen_bases': [2, 4, 1, 0], 'Defense_inning': [[27.0, 99.0, 8.0, 0, 0, 0, 0, 0], [0, 73.0, 32.0, 0, 0, 0, 0, 0], [0, 52.0, 14.0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]], 'Starting_games': [[3, 11, 0, 0, 0, 0, 0, 0], [0, 8, 4, 0, 0, 0, 0, 0], [0, 6, 2, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]], 'Winning_games': [[2, 3, 1, 0, 0, 0, 0, 0], [0, 6, 1, 0, 0, 0, 0, 0], [0, 2, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]};
+var pie_data24 = {'Catcher': ['大城', '小林', '岸田', 'その他'], 'stolen_bases': [2, 4, 1, 0], 'Defense_inning': [[27.0, 99.0, 8.0, 0, 0, 0, 0, 0], [0, 73.0, 32.0, 0, 0, 0, 0, 0], [0, 52.0, 23.0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]], 'Starting_games': [[3, 11, 0, 0, 0, 0, 0, 0], [0, 8, 4, 0, 0, 0, 0, 0], [0, 6, 3, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]], 'Winning_games': [[2, 3, 1, 0, 0, 0, 0, 0], [0, 6, 1, 0, 0, 0, 0, 0], [0, 2, 2, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]};
 var innings24 = pie_data24.Defense_inning;
 var starting_games24 = pie_data24.Starting_games;
 var stolen_bases24 = pie_data24.stolen_bases;
