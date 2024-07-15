6e497e893bbb66dcfebb890eb436eee1ba4eac65
kg1 <ki4017kk@gmail.com>
2024-07-15 20:53:46 +0900

    score update 2024.07.15

--- assets/demo/calendar-heatmap-demo-c.js
+++ assets/demo/calendar-heatmap-demo-c.js
@@ -82,6 +82,7 @@ var datas24 = {
     '2024-07-09': -1,
     '2024-07-12': 1,
     '2024-07-13': 1,
+    '2024-07-15': -1,
     //@@NEXTGAME@@
 };
 var score24 = {
@@ -164,6 +165,7 @@ var score24 = {
     '2024-07-09': 'vs Giants , 3-5 </br>Start P.森下 / C.會澤',
     '2024-07-12': 'vs Swallows , 4-3 </br>Start P.床田 / C.石原',
     '2024-07-13': 'vs Swallows , 1-0 </br>Start P.大瀬良 / C.會澤',
+    '2024-07-15': 'vs Baystars , 6-1 </br>Start P.アドゥワ / C.石原',
     //@@NEXTSCORE@@
 };
 var chartData24 = [];
