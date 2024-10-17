80c05f0c916b9f2e40c9f306c56aa5b1987065dc
kg1 <ki4017kk@gmail.com>
2024-10-17 19:01:52 +0900

    score update 2024.10.17

--- sho/sho-heatmap-demo.js
+++ sho/sho-heatmap-demo.js
@@ -170,6 +170,7 @@ var datas24 = {
 '2024-10-11': 0,
 '2024-10-13': 2,
 '2024-10-14': 0,
+'2024-10-16': 3,
 //@@TOOLTIP_DATE@@
 };
 var score24 = {
@@ -339,6 +340,7 @@ var score24 = {
     '2024-10-11': '<span><strong>0 HR / 0 SB</strong></span></br>0 Hits / 0 RBI / AVG .200',
     '2024-10-13': '<span><strong>0 HR / 0 SB</strong></span></br>2 Hits / 1 RBI / AVG .250',
     '2024-10-14': '<span><strong>0 HR / 0 SB</strong></span></br>0 Hits / 0 RBI / AVG .222',
+    '2024-10-16': '<span><strong>1 HR / 0 SB</strong></span></br>1 Hits / 3 RBI / AVG .226',
     //@@TOOLTIP@@
 };
 var chartData24 = [];
