d3f32fbfe37f1ca66da82b15ed21944b6e5cf06a
kg1 <ki4017kk@gmail.com>
2025-04-20 22:01:49 +0900

    udpate npb 2025.04.20

--- assets/demo/calendar-heatmap-demo-s.js
+++ assets/demo/calendar-heatmap-demo-s.js
@@ -18,6 +18,7 @@ var datas25 = {
     '2025-04-17': -1,
     '2025-04-18': -1,
     '2025-04-19': -1,
+    '2025-04-20': 1,
     //@@NEXTGAME@@
 };
 var score25 = {
@@ -36,6 +37,7 @@ var score25 = {
     '2025-04-17': 'vs Tigers , 5-7 </br>Start P.吉村 / C.松本直',
     '2025-04-18': 'vs Giants , 2-7 </br>Start P.小川 / C.中村',
     '2025-04-19': 'vs Giants , 4-6 </br>Start P.山野 / C.古賀',
+    '2025-04-20': 'vs Giants , 3-2 </br>Start P.ランバート / C.中村',
     //@@NEXTSCORE@@
 };
 var chartData25 = [];
