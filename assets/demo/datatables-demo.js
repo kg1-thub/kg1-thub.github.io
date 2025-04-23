fc0351fed1ec398b1a587c50c0cd3bb3c0d63a7b
kg1 <ki4017kk@gmail.com>
2025-04-23 15:00:31 +0900

    udpate sho 2025.04.22

--- assets/demo/datatables-demo.js
+++ assets/demo/datatables-demo.js
@@ -741,7 +741,7 @@ function makeCSV2(records, columns, year, keyword) {
         rowCallback: function(row, data, dataIndex) {
           $('td', row).each(function(i) {
             const cellText = $(this).text();
-            if (i>1 && (cellText.indexOf('安') !== -1 ||
+            if (i>1 && i<14 && (cellText.indexOf('安') !== -1 ||
               cellText.indexOf('２') !== -1 ||
               cellText.indexOf('３') !== -1 ||
               cellText.indexOf('本') !== -1)) {
