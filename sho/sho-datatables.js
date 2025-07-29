18893d98a5c0de9c2700e2bb4c8619640abcd561
kg1 <ki4017kk@gmail.com>
2025-07-29 11:13:20 +0900

    udpate sho 2025.07.28

--- sho/sho-datatables.js
+++ sho/sho-datatables.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     csvLoad(
         '25',
-        '2025-07-27' // @@KEYWORD@@
+        '2025-07-28' // @@KEYWORD@@
     );
     csvLoad2(
         '25',
--- sho/sho-heatmap-demo.js
+++ sho/sho-heatmap-demo.js
@@ -107,6 +107,7 @@ var datas25 = {
 '2025-07-25': 1,
 '2025-07-26': 3,
 '2025-07-27': 2,
+'2025-07-28': 3,
 //@@TOOLTIP_DATE@@
 };
 var score25 = {
@@ -215,6 +216,7 @@ var score25 = {
     '2025-07-25': '(Regular Season)</br><span><strong>0 HR / 0 SB</strong></span></br>1 Hits / 0 RBI / AVG .273',
     '2025-07-26': '(Regular Season)</br><span><strong>1 HR / 0 SB</strong></span></br>1 Hits / 1 RBI / AVG .273',
     '2025-07-27': '(Regular Season)</br><span><strong>0 HR / 0 SB</strong></span></br>2 Hits / 0 RBI / AVG .275',
+    '2025-07-28': '(Regular Season)</br><span><strong>0 HR / 0 SB</strong></span></br>1 Hits / 2 RBI / AVG .276',
     //@@TOOLTIP@@
 };
 var chartData25 = [];
@@ -264,6 +266,7 @@ var datas25p = {
 '2025-07-21': 1,
 '2025-07-21': 1,
 '2025-07-21': 1,
+'2025-07-21': 1,
 //@@TOOLTIP_DATE_P@@
 };
 var score25p = {
@@ -285,6 +288,7 @@ var score25p = {
     '2025-07-12': '(Regular Season)</br>3.0 IP / 36 NP / 0 R</br>---',
     '2025-07-12': '(Regular Season)</br>3.0 IP / 36 NP / 0 R</br>---',
     '2025-07-12': '(Regular Season)</br>3.0 IP / 36 NP / 0 R</br>---',
+    '2025-07-21': '(Regular Season)</br>3.0 IP / 46 NP / 1 R</br>---',
     '2025-07-21': '(Regular Season)</br>3.0 IP / 46 NP / 1 R</br>---',
     '2025-07-21': '(Regular Season)</br>3.0 IP / 46 NP / 1 R</br>---',
     '2025-07-21': '(Regular Season)</br>3.0 IP / 46 NP / 1 R</br>---',
