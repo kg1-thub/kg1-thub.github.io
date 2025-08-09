7c7e44cd25538229b80bb2e129d1684241e794e2
kg1 <ki4017kk@gmail.com>
2025-08-09 21:24:55 +0900

    udpate npb 2025.08.09

--- assets/demo/datatables-demo25s.js
+++ assets/demo/datatables-demo25s.js
@@ -1,12 +1,12 @@
 {
   csvLoad(
     '25',
-    '08/08', // @@KEYWORD@@
+    '08/09', // @@KEYWORD@@
     's'
   );
   csvLoad2(
     '25',
-    '08/08', // @@KEYWORD@@
+    '08/09', // @@KEYWORD@@
     's'
   );
 }
