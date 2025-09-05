41e91136df78c512d77248217809737f6f4429ba
kg1 <ki4017kk@gmail.com>
2025-09-05 23:13:40 +0900

    udpate npb 2025.09.05

--- assets/demo/datatables-demo25o.js
+++ assets/demo/datatables-demo25o.js
@@ -1,12 +1,12 @@
 {
   csvLoad(
     '25',
-    '09/04', // @@KEYWORD@@
+    '09/05', // @@KEYWORD@@
     'o'
   );
   csvLoad2(
     '25',
-    '09/04', // @@KEYWORD@@
+    '09/05', // @@KEYWORD@@
     'o'
   );
 }
