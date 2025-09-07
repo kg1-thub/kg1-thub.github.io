ba6bd97774f84e292c135b2e374d1f6f6ab6f89a
kg1 <ki4017kk@gmail.com>
2025-09-07 11:35:29 +0900

    udpate sho 2025.09.06

--- sho/sho-heatmap-demo.js
+++ sho/sho-heatmap-demo.js
@@ -141,6 +141,7 @@ var datas25 = {
 '2025-09-03': 2,
 '2025-09-04': 0,
 '2025-09-05': 0,
+'2025-09-06': 2,
 //@@TOOLTIP_DATE@@
 };
 var score25 = {
@@ -283,6 +284,7 @@ var score25 = {
     '2025-09-03': '(Regular Season)</br><span><strong>0 HR / 0 SB</strong></span></br>2 Hits / 0 RBI / AVG .280',
     '2025-09-04': '(Regular Season)</br><span><strong>0 HR / 0 SB</strong></span></br>0 Hits / 0 RBI / AVG .279',
     '2025-09-05': '(Regular Season)</br><span><strong>0 HR / 0 SB</strong></span></br>0 Hits / 0 RBI / AVG .277',
+    '2025-09-06': '(Regular Season)</br><span><strong>0 HR / 0 SB</strong></span></br>1 Hits / 1 RBI / AVG .276',
     //@@TOOLTIP@@
 };
 var chartData25 = [];
