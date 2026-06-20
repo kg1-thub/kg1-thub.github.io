25b543920ba7d3e470fb1e05e18442bb50782050
kg1 <ki4017kk@gmail.com>
2026-06-20 16:54:16 +0900

    npb 2026.6.20 updated.

--- assets/demo/calendar-heatmap-demo-o.js
+++ assets/demo/calendar-heatmap-demo-o.js
@@ -68,6 +68,7 @@ var datas26 = {
     '2026-06-13': -1,
     '2026-06-14': 1,
     '2026-06-19': 1,
+    '2026-06-20': -1,
     //@@NEXTGAME@@
 };
 var score26 = {
@@ -136,6 +137,7 @@ var score26 = {
     '2026-06-13': 'vs Tigers , 3-6 </br>Start P.ジェリー / C.若月',
     '2026-06-14': 'vs Tigers , 3-2 </br>Start P.九里 / C.若月',
     '2026-06-19': 'vs Lions , 6-5 </br>Start P.ｴｽﾋﾟﾉｰｻﾞ / C.若月',
+    '2026-06-20': 'vs Lions , 0-4 </br>Start P.ペルドモ / C.若月',
     //@@NEXTSCORE@@
 };
 var chartData26 = [];
--- assets/demo/datatables-demo26o.js
+++ assets/demo/datatables-demo26o.js
@@ -1,12 +1,12 @@
 {
   csvLoad(
     '26',
-    '06/19', // @@KEYWORD@@
+    '06/20', // @@KEYWORD@@
     'o'
   );
   csvLoad2(
     '26',
-    '06/19', // @@KEYWORD@@
+    '06/20', // @@KEYWORD@@
     'o'
   );
 }
