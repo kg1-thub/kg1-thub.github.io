5f1ae4a67183ddbd4f670b8bd36592875a7e8472
kg1 <ki4017kk@gmail.com>
2026-06-22 23:15:18 +0900

    npb 2026.6.22 updated.

--- assets/demo/calendar-heatmap-demo-l.js
+++ assets/demo/calendar-heatmap-demo-l.js
@@ -72,6 +72,7 @@ var datas26 = {
     '2026-06-19': -1,
     '2026-06-20': 1,
     '2026-06-21': 1,
+    '2026-06-22': -1,
     //@@NEXTGAME@@
 };
 var score26 = {
@@ -144,6 +145,7 @@ var score26 = {
     '2026-06-19': 'vs Buffaloes , 6-5 </br>Start P.高橋 / C.古賀悠',
     '2026-06-20': 'vs Buffaloes , 0-4 </br>Start P.隅田 / C.小島',
     '2026-06-21': 'vs Buffaloes , 3-5 </br>Start P.渡邉 / C.古賀悠',
+    '2026-06-22': 'vs Eagles , 8-7 </br>Start P.平良 / C.小島',
     //@@NEXTSCORE@@
 };
 var chartData26 = [];
