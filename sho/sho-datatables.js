d987715cabc76fa6f6d56df2f37dc9885c067a2f
kg1 <ki4017kk@gmail.com>
2026-06-30 13:49:39 +0900

    sho 2026.6.29 updated.

--- sho/sho-datatables.js
+++ sho/sho-datatables.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     csvLoad(
         '26',
-        '2026-06-28' // @@KEYWORD@@
+        '2026-06-29' // @@KEYWORD@@
     );
     csvLoad2(
         '26',
--- sho/sho-heatmap-demo.js
+++ sho/sho-heatmap-demo.js
@@ -79,6 +79,7 @@ var datas26 = {
 '2026-06-26': 1,
 '2026-06-27': 2,
 '2026-06-28': 2,
+'2026-06-29': 4,
 //@@TOOLTIP_DATE@@
 };
 var score26 = {
@@ -158,6 +159,7 @@ var score26 = {
     '2026-06-26': '(Regular Season)</br><span><strong>0 HR / 0 SB</strong></span></br>1 Hits / 0 RBI / AVG .294',
     '2026-06-27': '(Regular Season)</br><span><strong>0 HR / 0 SB</strong></span></br>2 Hits / 0 RBI / AVG .296',
     '2026-06-28': '(Regular Season)</br><span><strong>0 HR / 0 SB</strong></span></br>1 Hits / 1 RBI / AVG .295',
+    '2026-06-29': '(Regular Season)</br><span><strong>1 HR / 0 SB</strong></span></br>2 Hits / 3 RBI / AVG .297',
     //@@TOOLTIP@@
 };
 var chartData26 = [];
