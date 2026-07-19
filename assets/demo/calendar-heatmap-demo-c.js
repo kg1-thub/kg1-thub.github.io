1666d879bc0530dff565188321d387648d227497
kg1 <ki4017kk@gmail.com>
2026-07-19 21:27:13 +0900

    npb 2026.7.19 updated.

--- assets/demo/calendar-heatmap-demo-c.js
+++ assets/demo/calendar-heatmap-demo-c.js
@@ -85,6 +85,7 @@ var datas26 = {
     '2026-07-15': 1,
     '2026-07-17': -1,
     '2026-07-18': 1,
+    '2026-07-19': -1,
     //@@NEXTGAME@@
 };
 var score26 = {
@@ -170,6 +171,7 @@ var score26 = {
     '2026-07-15': 'vs Baystars , 6-4 </br>Start P.斉藤 / C.石原',
     '2026-07-17': 'vs Tigers , 2-4 </br>Start P.アドゥワ / C.石原',
     '2026-07-18': 'vs Tigers , 2-1 </br>Start P.栗林 / C.持丸',
+    '2026-07-19': 'vs Tigers , 2-4 </br>Start P.森 / C.持丸',
     //@@NEXTSCORE@@
 };
 var chartData26 = [];
--- assets/demo/calendar-heatmap-demo-t.js
+++ assets/demo/calendar-heatmap-demo-t.js
@@ -87,6 +87,7 @@ var datas26 = {
     '2026-07-16': 1,
     '2026-07-17': 1,
     '2026-07-18': -1,
+    '2026-07-19': 1,
     //@@NEXTGAME@@
 };
 var score26 = {
@@ -174,6 +175,7 @@ var score26 = {
     '2026-07-16': 'vs Dragons , 1-3 </br>Start P.伊原 / C.伏見',
     '2026-07-17': 'vs Carp , 2-4 </br>Start P.才木 / C.梅野',
     '2026-07-18': 'vs Carp , 2-1 </br>Start P.伊藤 / C.梅野',
+    '2026-07-19': 'vs Carp , 2-4 </br>Start P.村上 / C.坂本',
     //@@NEXTSCORE@@
 };
 var chartData26 = [];
