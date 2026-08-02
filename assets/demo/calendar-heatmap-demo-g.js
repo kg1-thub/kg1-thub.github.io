d5c6113f814fb145d187698475d1330d52835614
kg1 <ki4017kk@gmail.com>
2026-08-02 16:53:48 +0900

    npb 2026.8.2 updated.

--- assets/demo/calendar-heatmap-demo-g.js
+++ assets/demo/calendar-heatmap-demo-g.js
@@ -97,6 +97,7 @@ var datas26 = {
     '2026-07-26': -1,
     '2026-07-31': 1,
     '2026-08-01': -1,
+    '2026-08-02': -1,
     //@@NEXTGAME@@
 };
 var score26 = {
@@ -194,6 +195,7 @@ var score26 = {
     '2026-07-26': 'vs Tigers , 1-0 </br>Start P.小笠原 / C.山瀬',
     '2026-07-31': 'vs Baystars , 6-0 </br>Start P.ハワード / C.大城',
     '2026-08-01': 'vs Baystars , 7-8 </br>Start P.竹丸 / C.大城',
+    '2026-08-02': 'vs Baystars , 1-2 </br>Start P.小笠原 / C.大城',
     //@@NEXTSCORE@@
 };
 var chartData26 = [];
