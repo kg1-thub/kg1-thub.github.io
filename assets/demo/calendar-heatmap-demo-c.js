77318fde2c379220c0c7310ab03f4eb2a94d2ec4
kg1 <ki4017kk@gmail.com>
2026-08-18 22:06:43 +0900

    npb 2026.8.18 updated.

--- assets/demo/calendar-heatmap-demo-c.js
+++ assets/demo/calendar-heatmap-demo-c.js
@@ -105,6 +105,7 @@ var datas26 = {
     '2026-08-14': 1,
     '2026-08-15': 1,
     '2026-08-16': -1,
+    '2026-08-18': 1,
     //@@NEXTGAME@@
 };
 var score26 = {
@@ -210,6 +211,7 @@ var score26 = {
     '2026-08-14': 'vs Tigers , 6-1 </br>Start P.森下 / C.石原',
     '2026-08-15': 'vs Tigers , 5-2 </br>Start P.斉藤 / C.石原',
     '2026-08-16': 'vs Tigers , 1-8 </br>Start P.森 / C.石原',
+    '2026-08-18': 'vs Dragons , 11-4 </br>Start P.床田 / C.持丸',
     //@@NEXTSCORE@@
 };
 var chartData26 = [];
--- assets/demo/calendar-heatmap-demo-d.js
+++ assets/demo/calendar-heatmap-demo-d.js
@@ -112,6 +112,7 @@ var datas26 = {
     '2026-08-14': -1,
     '2026-08-15': 1,
     '2026-08-16': -1,
+    '2026-08-18': -1,
     //@@NEXTGAME@@
 };
 var score26 = {
@@ -224,6 +225,7 @@ var score26 = {
     '2026-08-14': 'vs Giants , 0-1 </br>Start P.高橋 / C.石伊',
     '2026-08-15': 'vs Giants , 11-2 </br>Start P.マラー / C.加藤',
     '2026-08-16': 'vs Giants , 0-11 </br>Start P.柳 / C.石伊',
+    '2026-08-18': 'vs Carp , 11-4 </br>Start P.大野 / C.石伊',
     //@@NEXTSCORE@@
 };
 var chartData26 = [];
