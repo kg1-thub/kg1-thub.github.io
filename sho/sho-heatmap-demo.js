19b26e9a3c0336b3f4aad0e2593a5fd95da5cd3d
kg1 <ki4017kk@gmail.com>
2026-09-26 17:29:29 +0900

    sho 2026.9.25 updated.

--- sho/sho-heatmap-demo.js
+++ sho/sho-heatmap-demo.js
@@ -134,6 +134,7 @@ var datas26 = {
 '2026-09-07': 0,
 '2026-09-23': 1,
 '2026-09-24': 3,
+'2026-09-25': 0,
 //@@TOOLTIP_DATE@@
 };
 var score26 = {
@@ -268,6 +269,7 @@ var score26 = {
     '2026-09-07': '(Regular Season)</br><span><strong>0 HR / 0 SB</strong></span></br>0 Hits / 0 RBI / AVG .277',
     '2026-09-23': '(Regular Season)</br><span><strong>0 HR / 0 SB</strong></span></br>1 Hits / 0 RBI / AVG .277',
     '2026-09-24': '(Regular Season)</br><span><strong>0 HR / 0 SB</strong></span></br>2 Hits / 1 RBI / AVG .278',
+    '2026-09-25': '(Regular Season)</br><span><strong>0 HR / 0 SB</strong></span></br>0 Hits / 0 RBI / AVG .276',
     //@@TOOLTIP@@
 };
 var chartData26 = [];
